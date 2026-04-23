dddb6dca554ae7449f79478bde1e52d3efc294a7
Atilla222 <132841698+Somatilla@users.noreply.github.com>
2026-04-23 19:38:02 +0200

    Enhance file management commands in main.py
    Added commands for copying files and reading text files. Improved help command output and also fixed some pesky bugz.

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ running = True
 
 print("  ______   ________  __  __                      _______  __      __ ")
 print(" /      \\ |        \\|  \\|  \\                    |       \\|  \\    /  \\")
-print("|  $$$$$$\| $$$$$$$$ \$$| $$  ______    _______ | $$$$$$$\\$$\  /  $$")
-print("| $$__| $$| $$__    |  \| $$ /      \  /       \| $$__/ $$ \$$\/  $$ ")
-print("| $$    $$| $$  \   | $$| $$|  $$$$$$\|  $$$$$$$| $$    $$  \$$  $$  ")
-print("| $$$$$$$$| $$$$$   | $$| $$| $$    $$ \$$    \ | $$$$$$$    \$$$$   ")
-print("| $$  | $$| $$      | $$| $$| $$$$$$$$ _\$$$$$$\| $$         | $$    ")
-print("| $$  | $$| $$      | $$| $$ \$$     \|       $$| $$         | $$    ")
-print(" \$$   \$$ \$$       \$$ \$$  \$$$$$$$ \$$$$$$$  \$$          \$$    ")
-
+print("|  $$$$$$\\| $$$$$$$$ \\$$| $$  ______    _______ | $$$$$$$\\\\$$\\  /  $$")
+print("| $$__| $$| $$__    |  \\| $$ /      \\  /       \\| $$__/ $$ \\$$\\/  $$ ")
+print("| $$    $$| $$  \\   | $$| $$|  $$$$$$\\|  $$$$$$$| $$    $$  \\$$  $$  ")
+print("| $$$$$$$$| $$$$$   | $$| $$| $$    $$ \\$$    \\ | $$$$$$$    \\$$$$   ")
+print("| $$  | $$| $$      | $$| $$| $$$$$$$$ _\\$$$$$$\\| $$         | $$    ")
+print("| $$  | $$| $$      | $$| $$ \\$$     \\|       $$| $$         | $$    ")
+print(" \\$$   \\$$ \\$$       \\$$ \\$$  \\$$$$$$$ \\$$$$$$$  \\$$          \\$$    ")
+print("")
+print("type help for command list")
+print("")
 
 while running:
     command = input("fm>")
@@ -30,15 +32,45 @@ while running:
         print(f"Renamed {Splitted[1]} to {Splitted[2]}")
     
     elif Splitted[0] == "crf":
-        os.makedirs(Splitted[1] + Splitted[2], exist_ok=True)
+        os.makedirs(Splitted[1] + "/" + Splitted[2], exist_ok=True)
         print(f"Created {Splitted[2]} at {Splitted[1]}")
     
     elif Splitted[0] == "del":
         file = Path(Splitted[1] + "/" + Splitted[2])
         if file.exists() == True:
-            file.unlink(file)
+            os.remove(file)
         else:
             print("file doesnt exist")
+    
+    elif Splitted[0] == "cp":
+        cpfolder = Splitted[1]
+        cpfile = Splitted[2]
+
+        print(f"Copied{cpfolder}/{cpfile}")
+    
+    elif Splitted[0] == "ps":
+        pfolder = Splitted[1]
+        pfile = Splitted[2]
+
+        shutil.copyfile(cpfolder + "/" + cpfile, pfolder + "/" + pfile)
+
+        print(f"Pasted {cpfolder}/{cpfile} to {pfolder}/{pfile}")
+
+    elif Splitted[0] == "rdtxt":
+        print(open(Splitted[1] + "/" + Splitted[2]).read())
+        print("end of file")
+    
+    elif Splitted[0] == "help":
+        print("commands:")
+        print("mv <directory/file> <directory2/file> -- moves <directory/file> to <directory2/file>")
+        print("ren <directory/file> <directory/file2> -- renames <directory/file> to <directory/file2>")
+        print("crf <folder> <newfolder> -- creates a folder at <folder>")
+        print("del <directory> <file> -- deletes <file> at <director>")
+        print("cp <directory> <file> -- copies <file> at <directory>")
+        print("ps <folder> <file> pastes copied files to <folder/file>")
+        print("rdtxt <directory> <file> -- reads <file> at <directory>")
+        print("ls <directory> lists all files in <directory>")
+        print("quit -- quits AFilesPY")
 
     elif Splitted[0] == "ls":
         path = Splitted[1] if len(Splitted) > 1 else "."
